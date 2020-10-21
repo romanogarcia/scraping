b5a95617388309d3ec74ed9b0630f7066f6c4107
Roman Garcia <romanogarcia.ph@gmail.com>
2020-10-21 10:44:50 +0800

    Insert record to database

--- get-sitemap.php
+++ get-sitemap.php
@@ -1,26 +1,18 @@
 <?php
-// $servername = "localhost";
-// $dbname = "recipesearchdb";
-// $username = "recipesearchsql";
-// $password = "pIfanlwathuS0utr";
 
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
+$servername = "localhost";
+$dbname = "recipesearchdb";
+$username = "root"; //"recipesearchsql";
+$password = "Roman123456"; //"pIfanlwathuS0utr";
+$tablename = "spendwithpennies.com";
 
-// $sql = "INSERT INTO MyGuests (firstname, lastname, email)
-// VALUES ('John', 'Doe', 'john@example.com');";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
-// if ($conn->multi_query($sql) === TRUE) {
-//   echo "New records created successfully";
-// } else {
-//   echo "Error: " . $sql . "<br>" . $conn->error;
-// }
-
-// $conn->close();
 
 $context = stream_context_create(
     array(
@@ -38,27 +30,47 @@ $context = stream_context_create(
 // $url = "https://www.cookingclassy.com/sitemap_index.xml";
 // $url = "https://www.skinnytaste.com/sitemap_index.xml";
 // $url = "https://www.pillsbury.com/sitemap.xml"; // not ldjson schema
-// $url = "https://www.spendwithpennies.com/sitemap_index.xml";
-// $url = "https://www.simplyrecipes.com/sitemap.xml";
-$url = "https://www.allrecipes.com/sitemap.xml";
-
+$url = "https://www.spendwithpennies.com/sitemap_index.xml";
+// $url = "https://www.simplyrecipes.com/sitemap.xml"; //forbidden error
+// $url = "https://www.allrecipes.com/sitemap.xml";
+$scrape_date = date('Y-m-d');
+$date_created = date('Y-m-d');
+$scrape = "NO";
 $myXMLData = file_get_contents($url);
+// $contain_schema = 'type":"Recipe';
+$contain_schema = 'type="application/ld+json"';
 
 $xml = simplexml_load_string($myXMLData) or die("Error: Cannot create object");
 // print_r($xml);die();
 foreach ($xml as $val){
 	foreach($val->loc as $loc) {
-		
 		// $locXml = file_get_contents($loc);
 		$locXml = file_get_contents($loc, false, $context);
 		$dataXml = simplexml_load_string($locXml);
 		 
 		foreach($dataXml as $val){
-			echo $val->loc[0] . PHP_EOL;
+			$url = $val->loc[0];
+			$content = file_get_contents($val->loc[0]);
+			if(strpos($content, '@type": "Recipe') !== false || strpos($content, '@type":"Recipe') !== false ){
+			     // echo "Found " .  $val->loc[0] . PHP_EOL;
+				// $sql = "INSERT INTO `$tablename` (url, scrape, scrape_date, date_created)
+				// VALUES (`$url`, 'NO', `$scrape_date`, `$date_created` );";
+
+$sql = "INSERT INTO `$tablename` (url, scrape, scrape_date, date_created)
+    VALUES ('".$url."', '".$scrape."', '".$scrape_date."', '".$date_created."')";
+
+				if ($conn->multi_query($sql) === TRUE) {
+				  echo "New records created successfully -> " . $val->loc[0] . PHP_EOL;
+				} else {
+				  echo "Error: " . $sql . "<br>" . $conn->error;
+				}
+
+			} else{
+			    echo "Not Found " .  $val->loc[0] . PHP_EOL; 
+			}
 		}
+
 	}
 }
 
-function getUrlLoc($array){
-	
-}
+$conn->close();
